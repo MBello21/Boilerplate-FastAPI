0505236e16b52b16f0ab401bffc141bd532e942d
MBello21 <magarbello@gmail.com>
2026-07-13 12:03:24 +0000

    Creating

---
+++ src/api/router_global.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from src.api.user.routes import router as users_router
+
+api_router = APIRouter()
---
+++ src/api/users/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    username: str
+    is_active: bool
+
+    class Config:
+        from_atributes = True
